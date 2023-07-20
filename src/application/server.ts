810bc7b79b3d40958ef91de995be69c1f491d82c
Ricardo Kühlkamp da Silva <103215010+ricardokuhlkamp@users.noreply.github.com>
2023-07-20 14:18:15 -0300

    Update server.ts

--- src/application/server.ts
+++ src/application/server.ts
@@ -2,10 +2,10 @@ import 'dotenv/config';
 import app from './app';
 import connectToDatabase from '../infrastructure/models/Connection';
 
-const PORT = process.env.PORT || 3001;
+const PORT = +process.env.PORT || 3001;
 connectToDatabase()
   .then(() => {
-    app.listen(PORT, () => console.log(`Running server on port: ${PORT}`));
+    app.listen(PORT, '0.0.0.0', () => console.log(`Running server on port: ${PORT}`));
   })
   .catch((error) => {
     console.log('Connection with database generated an error:\r\n');
